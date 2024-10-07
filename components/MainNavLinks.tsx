400971a0b775c2e8249c3adfe59de6ebd50340b8
Alejandro Romero <alexrom789@gmail.com>
2024-10-07 12:07:46 -0400

    initialize-layout-and-theme

---
+++ components/MainNavLinks.tsx
@@ -0,0 +1,34 @@
+"use client";
+import React from "react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const MainNavLinks = () => {
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Kanji", href: "/tickets" },
+    { label: "Users", href: "/users" },
+  ];
+
+  const currentPath = usePathname();
+  return (
+    <div className="flex items-center gap-4">
+      <Link href="/" className="mr-4">
+        KWIZZLE
+      </Link>
+      {links.map((link) => (
+        <Link
+          href={link.href}
+          className={`navbar-link ${
+            currentPath == link.href && "cursor-default text-primary"
+          }`}
+          key={link.label}
+        >
+          {link.label}
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default MainNavLinks;
