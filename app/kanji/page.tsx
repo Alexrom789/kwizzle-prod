69e2774d1d28d3d151cfc6cdc5a5bef8e0262e8a
Alejandro Romero <alexrom789@gmail.com>
2024-10-07 22:09:10 -0400

    add-server-pagination

--- app/kanji/page.tsx
+++ app/kanji/page.tsx
@@ -1,8 +1,17 @@
 import React from "react";
 import DataTable from "./DataTable";
 import prisma from "@/prisma/db";
+import Pagination from "@/components/Pagination";
 
-const Kanji = async () => {
+interface SearchParams {
+  page: string;
+}
+
+const Kanji = async ({ searchParams }: { searchParams: SearchParams }) => {
+  const pageSize = 10;
+  const page = parseInt(searchParams.page) || 1;
+  const kanjiCount = await prisma.kanji.count();
+
   const kanji = await prisma.kanji.findMany({
     include: {
       kunyomi: true,
@@ -10,14 +19,28 @@ const Kanji = async () => {
       meanings: true,
       similarKanji: true,
     },
+
+    take: pageSize,
+    skip: (page - 1) * pageSize,
   });
   const users = await prisma.user.findMany({
     include: {
       kanjiProgress: true,
     },
   });
+
   return (
-    <DataTable kanji={kanji} kanjiProgress={users[0].kanjiProgress}></DataTable>
+    <div>
+      <DataTable
+        kanji={kanji}
+        kanjiProgress={users[0].kanjiProgress}
+      ></DataTable>
+      <Pagination
+        itemCount={kanjiCount}
+        pageSize={pageSize}
+        currentPage={page}
+      />
+    </div>
   );
 };
 
